fbcdb491182de620457e4ce42057b5caad1771c7
vijayz96 <vijuxpsy@gmail.com>
2026-06-23 20:14:27 +0530

    Delete imagetest.js

--- plugins/imagetest.js
+++
@@ -1,8 +0,0 @@
-const Asena = require("../Utilis/events");
-
-Asena.addCommand(
-  { on: "image", fromMe: false },
-  async (message) => {
-    await message.sendMessage("📸 Image detected!");
-  }
-);
